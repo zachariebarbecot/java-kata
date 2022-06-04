b572fd7cae1aae3f797aa435c397fae58a64c403
Zacharie BARBECOT <z.barbecot@gmail.com>
2022-06-04 12:59:53 +0200

    leap year refacto

--- leap-year/src/main/java/fr/zbar/kata/leapyear/Year.java
+++ leap-year/src/main/java/fr/zbar/kata/leapyear/Year.java
@@ -1,6 +1,8 @@
 package fr.zbar.kata.leapyear;
 
 import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.function.Predicate;
 
 public record Year(BigInteger value) {
 
@@ -9,12 +11,27 @@ public record Year(BigInteger value) {
     private static final BigInteger FOUR_HUNDRED = HUNDRED.multiply(FOUR);
 
     public static Boolean isLeap(Year year) {
-        return year.isDivisibleBy(FOUR_HUNDRED) ||
-                (year.isDivisibleBy(FOUR) && !year.isDivisibleBy(HUNDRED));
+        return Arrays.stream(LeapYearRules.values())
+                .anyMatch(rule -> rule.verify(year));
     }
 
     private Boolean isDivisibleBy(BigInteger divisor) {
         return value.mod(divisor)
                 .equals(BigInteger.ZERO);
+    }
+
+    private enum LeapYearRules {
+        DIVISIBLE_BY_400(year -> year.isDivisibleBy(FOUR_HUNDRED)),
+        DIVISIBLE_BY_4_AND_NOT_BY_HUNDRED(year -> year.isDivisibleBy(FOUR) && !year.isDivisibleBy(HUNDRED));
+
+        private final Predicate<Year> rule;
+
+        LeapYearRules(Predicate<Year> rule) {
+            this.rule = rule;
+        }
+
+        public Boolean verify(Year year) {
+            return rule.test(year);
+        }
     }
 }
