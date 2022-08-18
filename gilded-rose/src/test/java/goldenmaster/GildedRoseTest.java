d7ae7dadd5976a5800e9c5891fdb6abf2fad96e3
Zacharie BARBECOT <z.barbecot@gmail.com>
2022-08-18 13:03:56 +0200

    add feature/gilded-rose

---
+++ gilded-rose/src/test/java/goldenmaster/GildedRoseTest.java
@@ -0,0 +1,19 @@
+package goldenmaster;
+
+import fr.zbar.kata.gildedrose.GildedRose;
+import fr.zbar.kata.gildedrose.Item;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GildedRoseTest {
+
+    @Test
+    void foo() {
+        Item[] items = new Item[]{new Item("foo", 0, 0)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("fixme", app.items[0].name);
+    }
+
+}
